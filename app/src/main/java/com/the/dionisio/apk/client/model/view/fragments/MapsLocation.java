6a100455345901977890e43cdfe2a23bc989067e
Diego Dantas <diego.dsdantas@gmail.com>
2017-05-21 18:14:28 -0300

    Scope: Implementation of map | Status: done

--- app/src/main/java/com/the/dionisio/apk/client/model/view/fragments/MapsLocation.java
+++ app/src/main/java/com/the/dionisio/apk/client/model/view/fragments/MapsLocation.java
@@ -3,11 +3,9 @@ package com.the.dionisio.apk.client.model.view.fragments;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,19 +16,16 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.the.dionisio.apk.client.model.dto.EventDTO;
 import com.the.dionisio.apk.client.model.view.Event;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class MapsLocation extends SupportMapFragment implements OnMapReadyCallback,
-        GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener
+        GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener
 {
     private static final String TAG = "Maps";
     private GoogleMap mMap;
     private LocationManager locationManager;
-    private String lat;
-    private String log;
     private String title;
-    MarkerOptions markerLocation = new MarkerOptions();
     List<EventDTO> eventDTOs;
 
 
@@ -44,8 +39,8 @@ public class MapsLocation extends SupportMapFragment implements OnMapReadyCallba
         eventDTOs = (List<EventDTO>) getActivity().getIntent().getSerializableExtra("ListEvent");
 
 
-        //markerLocation = markerEvet(eventDTOs).get(0);
     }
+
 
     /**
      * Manipulates the map once available.
@@ -58,27 +53,29 @@ public class MapsLocation extends SupportMapFragment implements OnMapReadyCallba
      */
 
 
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
         try{
 
-            // locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-            //  Criteria criteria = new Criteria();
-
-            //  String provaider = locationManager.getBestProvider(criteria, true);
-            //  Toast.makeText(getActivity(), "Provider " + provaider, Toast.LENGTH_LONG).show();
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
             mMap = googleMap;
+            mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setZoomControlsEnabled(true);
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+
             markerEvet(eventDTOs);
 
 
-            //mMap.setOnMapClickListener(this);
+            mMap.setOnMapClickListener(this);
+            //mMap.setOnMarkerClickListener(this);
+            mMap.setOnInfoWindowClickListener(this);
 
-            // mMap.setMyLocationEnabled(true);
 
-            //  mMap.setOnMarkerClickListener(this);
         }catch(SecurityException ex){
             Log.e(TAG, "Error: ", ex);
         }
@@ -87,9 +84,7 @@ public class MapsLocation extends SupportMapFragment implements OnMapReadyCallba
     @Override
     public void onMapClick(LatLng latLng) {
         MarkerOptions markerOptions = new MarkerOptions();
-
         markerOptions.getIcon();
-
         AlertDialog.Builder al
                 = new AlertDialog.Builder(getContext());
         al.setMessage("vamos brincar: ");
@@ -97,6 +92,7 @@ public class MapsLocation extends SupportMapFragment implements OnMapReadyCallba
         al.show();
     }
 
+    //Event screen call
     @Override
     public boolean onMarkerClick(Marker marker) {
         Intent i = new Intent(getContext(), Event.class);
@@ -111,7 +107,6 @@ public class MapsLocation extends SupportMapFragment implements OnMapReadyCallba
         double lati, longi;
         MarkerOptions marker = new MarkerOptions();
 
-
         for(EventDTO ev : listEvent){
             lati = Double.parseDouble(ev.latitude);
             longi = Double.parseDouble(ev.longitude);
@@ -122,12 +117,16 @@ public class MapsLocation extends SupportMapFragment implements OnMapReadyCallba
             marker.position(location);
             marker.title(title);
             mMap.addMarker(marker);
-
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
         }
-
-
     }
 
-
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Intent i = new Intent(getContext(), Event.class);
+        i.putExtra("LOCAL", marker.getTitle().toString());
+        i.putExtra("LATLOG", marker.getPosition().toString());
+        startActivity(i);
+    }
 
 }
